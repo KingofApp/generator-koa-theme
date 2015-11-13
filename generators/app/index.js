16fd2008293eca6c3964060f8bf3f9a21e674cdd
Abdón Rodríguez Davila <a@abdonrd.com>
2015-11-13 13:05:54 +0100

    Add the basic template structure

--- generators/app/index.js
+++ generators/app/index.js
@@ -8,6 +8,31 @@ module.exports = yeoman.generators.NamedBase.extend({
     this.themeName = this.name;
     this.themeKoaName = 'koa-' + this.themeName + '-theme';
     this.destinationRoot(this.destinationPath() + '/_' + this.themeKoaName);
+  },
+
+  writing: function() {
+    this.fs.copyTpl(
+      this.templatePath('bower.json'),
+      this.destinationPath('bower.json'),
+      {themeName: this.themeName, themeKoaName: this.themeKoaName}
+    );
+
+    this.fs.copyTpl(
+      this.templatePath('koa-theme.html'),
+      this.destinationPath(this.themeKoaName + '.html'),
+      {themeName: this.themeName}
+    );
+
+    this.fs.copyTpl(
+      this.templatePath('dist/koa-theme.html'),
+      this.destinationPath('dist/' + this.themeKoaName + '.html'),
+      {themeName: this.themeName}
+    );
+
+    this.directory(
+      this.templatePath('styles'),
+      this.destinationPath('styles')
+    );
   }
 
 });
