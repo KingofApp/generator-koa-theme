2a3ca4408951e84bff7bf4e5bddb8a8536dd6bdf
Abdón Rodríguez Davila <a@abdonrd.com>
2015-11-13 13:04:08 +0100

    Create a init function for create names and paths

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,5 +4,10 @@ var yeoman = require('yeoman-generator');
 var tags = require('./tags.json');
 
 module.exports = yeoman.generators.NamedBase.extend({
+  init: function() {
+    this.themeName = this.name;
+    this.themeKoaName = 'koa-' + this.themeName + '-theme';
+    this.destinationRoot(this.destinationPath() + '/_' + this.themeKoaName);
+  }
 
 });
