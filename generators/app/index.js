3c5f9f2911b4afcf853cfcb0c0c7b9c0e75104c3
Abdón Rodríguez Davila <a@abdonrd.com>
2015-11-13 13:06:16 +0100

    Add all elements

--- generators/app/index.js
+++ generators/app/index.js
@@ -33,6 +33,40 @@ module.exports = yeoman.generators.NamedBase.extend({
       this.templatePath('styles'),
       this.destinationPath('styles')
     );
-  }
+  },
 
+  writingElements: function() {
+    tags.forEach(function(tag) {
+      var isSubElement = tag.search('/') !== -1;
+
+      if (!isSubElement) {
+        var tagName = this.themeName + '-' + tag;
+
+        this.fs.copyTpl(
+          this.templatePath('elements/koa-' + tag + '/koa-' + tag + '.html'),
+          this.destinationPath('elements/' + tagName + '/' + tagName + '.html'),
+          {tagName: tagName, themeName: this.themeName}
+        );
+
+        this.fs.copyTpl(
+          this.templatePath('elements/demo.html'),
+          this.destinationPath('elements/' + tagName + '/demo/index.html'),
+          {tagName: tagName}
+        );
+      } else {
+        var parentTag = tag.split('/')[0];
+        var childTag = tag.split('/')[1];
+        var parentTagName = this.themeName + '-' + parentTag;
+        var tagName = this.themeName + '-' + childTag;
+        var tPath = 'elements/koa-' + parentTag + '/koa-' + childTag + '.html';
+        var dPath = 'elements/' + parentTagName + '/' + tagName + '.html';
+
+        this.fs.copyTpl(
+          this.templatePath(tPath),
+          this.destinationPath(dPath),
+          {tagName: tagName, themeName: this.themeName}
+        );
+      }
+    }.bind(this));
+  }
 });
